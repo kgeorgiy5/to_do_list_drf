e349b3fefca5bb6e43f505ad8ab333ca8a078d96
Nazariy Soloviy <nazarsoloviy2@gmail.com>
2023-08-18 17:27:48 +0100

    started work on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [tasks, setTasks] = useState([]); // Initialize with an empty array
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/tasks-get')
+      .then(res => res.json())
+      .then(data => {
+        setTasks(data);
+      })
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <>
-
+      <h1>Array of Objects</h1>
+      <ul>
+        {tasks.map((obj, index) => (
+          <li key={index}>
+            {obj.title}
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
